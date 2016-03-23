c2af463981aae1005e2eee06f8b9be62b388fbad
Stratos Kamadanis <kam.stratos@gmail.com>
2016-03-23 10:47:21 +0200

    Added GET support.

--- js/formHandler.js
+++ js/formHandler.js
@@ -1,9 +1,49 @@
 function handleEnter(event) {
     if (event.keyCode === 13) {
-        formHandle();
+        setGET();
+    }
+    return false;
+}
+
+function setGET(){
+    var params="?q="+$('#inputText').val();
+    $('#form').find('input:checked').each(function () {
+        params=params+"&"+$(this).attr('name')+"=1";
+    });
+    $(".custom-indeterminate").each(function () {
+        params=params+"&"+$(this).attr('for')+"=0";
+    });
+    if (!location.origin)
+        location.origin = location.protocol + "//" + location.host;
+    window.location = location.protocol +'//'+ location.host + location.pathname+params;
+}
+
+function handleLoad(){
+    var params=window.location.search.substring(1).split("&");
+    if(params==""){
+        $('#form').find('input:checkbox').prop("checked", true).next('label').removeClass("custom-unchecked").addClass("custom-checked");
+    }else{
+        populateForm(params);
+        document.getElementById("categories").className = "collapse";
+    //    TODO backend query
     }
 
-    return false;
+}
+
+function populateForm(params){
+    var i;
+    for (i = 0; i < params.length; ++i) {
+        var param=params[i].split("=");
+        if(param[0]=="q"){
+            document.getElementById("inputText").value=param[1];
+        }else{
+            if(param[1]==0){
+                $("#"+param[0]).next('label').removeClass("custom-unchecked").addClass("custom-indeterminate");
+            }else if(param[1]==1){
+                $("#"+param[0]).prop("checked", true).next('label').removeClass("custom-unchecked").addClass("custom-checked");
+            }
+        }
+    }
 }
 
 function formHandle() {
